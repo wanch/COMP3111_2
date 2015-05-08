4053d9d73a5fc25f529a3fdc28d82d8a7fef4b7c
wanch <wanch@users.noreply.github.com>
2015-05-08 21:14:19 +0800

    debugg final version

--- src/hkust/cse/calendar/unit/Location.java
+++ src/hkust/cse/calendar/unit/Location.java
@@ -28,6 +28,6 @@ public class Location implements Serializable {
 	}
 	
 	public String toString() {
-		return mLocationName + " [" + mCapacity + "]";
+		return mLocationName + "\t\t [" + mCapacity + "]";
 	}
 }
